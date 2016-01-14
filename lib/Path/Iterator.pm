fbfcbe83ff7f8bb870cad272acddd908fc402ba6
Leon Timmermans <fawaka@gmail.com>
2016-01-14 01:04:44 +0100

    Always run not() last

--- lib/Path/Iterator.pm
+++ lib/Path/Iterator.pm
@@ -8,7 +8,8 @@ my %priority = (
 	0 => <depth skip-hidden>,
 	1 => <skip skip-dir skip-subdir skip-vcs>,
 	2 => <name ext>,
-	4 => <content line-match shebang>
+	4 => <content line-match shebang>,
+	5 => <not>
 ).flatmap: { ($_ => $^pair.key for @($^pair.value)) };
 
 our sub finder(*%options) is export(:find) {
